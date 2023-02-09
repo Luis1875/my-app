34d2b79c42075df83ce02c2b5c6a766136905c3d
Luis1875 <mustanglans@gmail.com>
2023-02-08 19:37:37 -0600

    Nombre del proyecto: my-app. Description: version final del proyecto my-app

---
+++ src/main/java/com/lans/myapp/entiy/TestEntity.java
@@ -0,0 +1,14 @@
+package com.lans.myapp.entiy;
+
+public class TestEntity {
+	
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+private String nombre;
+}
